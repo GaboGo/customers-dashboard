61a59a9726d002f80fb46adb49380eda49c24641
Gabriel Goethe <gabogo@MacBook-Air-de-Gabriel.local>
2025-03-22 13:22:18 -0500

    feat: add customers page table

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,9 +1,36 @@
+import Table from "@/app/ui/customers/table";
+import { lusitana } from "@/app/ui/fonts";
+import { InvoicesTableSkeleton } from "@/app/ui/skeletons";
+import { Suspense } from "react";
 import { Metadata } from "next";
+import { fetchCustomers, fetchFilteredCustomers } from "@/app/lib/data";
 
 export const metadata: Metadata = {
   title: "Customers",
 };
 
-export default function Page() {
-  return <p>Customers Page</p>;
+export default async function Page(props: {
+  searchParams?: Promise<{
+    query?: string;
+    page?: string;
+  }>;
+}) {
+  const searchParams = await props.searchParams;
+  const query = searchParams?.query || "";
+  const currentPage = Number(searchParams?.page) || 1;
+  const filteredCustomers = await fetchFilteredCustomers(query, currentPage);
+  const customers = await fetchCustomers();
+  return (
+    <div className="w-full">
+      <div className="mt-4 flex items-center justify-between gap-2 md:mt-8">
+        {/* <Search placeholder="Search customers..." /> */}
+      </div>
+      <Suspense key={query + currentPage} fallback={<InvoicesTableSkeleton />}>
+        <Table customers={filteredCustomers} />
+      </Suspense>
+      <div className="mt-5 flex w-full justify-center">
+        {/* <Pagination totalPages={totalPages} /> */}
+      </div>
+    </div>
+  );
 }
