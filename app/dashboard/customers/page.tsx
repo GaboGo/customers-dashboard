5872ec13fe269ca22b5489ab6523fad4aa92635c
Gabriel Goethe <gabogo@MacBook-Air-de-Gabriel.local>
2025-03-23 11:09:18 -0500

    feat: update customers page

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,9 +1,8 @@
 import Table from "@/app/ui/customers/table";
-import { lusitana } from "@/app/ui/fonts";
 import { InvoicesTableSkeleton } from "@/app/ui/skeletons";
 import { Suspense } from "react";
 import { Metadata } from "next";
-import { fetchCustomers, fetchFilteredCustomers } from "@/app/lib/data";
+import { fetchFilteredCustomers } from "@/app/lib/data";
 
 export const metadata: Metadata = {
   title: "Customers",
@@ -19,18 +18,11 @@ export default async function Page(props: {
   const query = searchParams?.query || "";
   const currentPage = Number(searchParams?.page) || 1;
   const filteredCustomers = await fetchFilteredCustomers(query, currentPage);
-  const customers = await fetchCustomers();
   return (
     <div className="w-full">
-      <div className="mt-4 flex items-center justify-between gap-2 md:mt-8">
-        {/* <Search placeholder="Search customers..." /> */}
-      </div>
       <Suspense key={query + currentPage} fallback={<InvoicesTableSkeleton />}>
         <Table customers={filteredCustomers} />
       </Suspense>
-      <div className="mt-5 flex w-full justify-center">
-        {/* <Pagination totalPages={totalPages} /> */}
-      </div>
     </div>
   );
 }
